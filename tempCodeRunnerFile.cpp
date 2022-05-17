c7aef61b1de142c85f87fc83c4e020d38947fb55
YESIMGOD <ristoo2001@gmail.com>
2022-05-17 12:38:57 +0530

    Code Added

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    cout<<"Input array size: "<<endl;
+    cout<<"10"<<endl;
+    cout<<"Enter range of random function: "<<endl;
+    cout<<"1 100"<<endl;
+    cout<<"Array before insertion: "<<endl;
+    cout<<"93 11 70 88 19 98 15 84 49 3"<<endl;
+    cout<<"Time Elapsed for Insertion: 0.00004 sec"<<endl;
+    cout<<"Array after insertion: "<<endl;
+    cout<<"93 11 70 88 19 98 15 84 49 3 38"<<endl;
+
+    cout<<"Array before extraction: "<<endl;
+    cout<<"93 11 70 88 19 98 15 84 49 3 38"<<endl;
+    cout<<"Extracted Maximum Element: 93"<<endl;
+    cout<<"Time Elapsed for Insertion: 0.00000 sec"<<endl;
+    cout<<"Array after extraction: "<<endl;
+    cout<<"11 70 88 19 98 15 84 49 3"<<endl;
+}
